b7c1a891495059539d5f2959749042234bd8e55b
Moojob <moudiop123@gmail.com>
2022-07-07 20:53:25 +0000

    mis

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,7 @@
     var timeRemaining = parseInt((dateEnd - dateStart.getTime()) / 1000)
 
     if ( timeRemaining >= 0 ) {
-      days    = parseInt(timeRemaining / 86400);
+      days    = parseInt(timeRemaining / 864000);
       timeRemaining   = (timeRemaining % 86400);
       hours   = parseInt(timeRemaining / 3600);
       timeRemaining   = (timeRemaining % 3600);
@@ -29,7 +29,7 @@
       timeRemaining   = (timeRemaining % 60);
       seconds = parseInt(timeRemaining);
 
-      document.getElementById("days").innerHTML    = parseInt(days, 9);
+      document.getElementById("days").innerHTML    = parseInt(days, 25);
       document.getElementById("hours").innerHTML   = ("0" + hours).slice(-2);
       document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
       document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
