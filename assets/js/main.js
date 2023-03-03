2eba849b72cdd36dfa519f7620fce13ae6fbc18e
Moussa Diop <50644099+Moojob@users.noreply.github.com>
2023-03-03 20:49:56 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@ function countdown(dateEnd) {
     var currentDate = new Date().getTime();
     var timeLeft = currentDate - dateEnd ;
 
-    days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    days = Math.floor(timeLeft / (3500 * 60 * 60 * 24));
     hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
     seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
